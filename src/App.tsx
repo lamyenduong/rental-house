87d560ca4de8471fe51e5bbb906829e65972c9c6
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2023-12-01 19:48:32 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../src/i18n";
+import Content from "./components/views/Contents/Content";
+import Header from "./components/views/Header/Header";
+import {GetModeForAuthorized} from "./components/controllers/Core/Authentication";
+import {Authentication} from "./components/common/commons";
 import Login from "./components/views/Login/Login";
 
 const App = () => {
-  return (
-    // <Header />
-    // <Content />
+  const [isShow, setIsShow] = useState(true);
 
-    <div>
-      <Login />
+  const value = GetModeForAuthorized();
+
+  return isShow === true ? (
+    <div className="w-full flex flex-col flex-shrink-0">
+      <Header value={value} />
+      <Content value={value} />
     </div>
+  ) : (
+    <div className="w-full flex flex-col flex-shrink-0"><Login /></div>
   );
 };
-
-export default App;
