90b3755c04970477fd4b1929556b1dce11b40306
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2023-12-01 19:35:59 +0700

    Add files via upload

--- src/components/common/commons.ts
+++ src/components/common/commons.ts
@@ -6,3 +6,31 @@ export const enum Colors {
   blackish_gray = "#333333",
   light_gray = "#D9D9D9",
 }
+
+export const enum Authentication {
+  isOwnerMode = "Owner",
+  isTenantsMode = "Tenants",
+}
+
+export const enum AccessSession {
+  isLogin = 0,
+  isLogOut = 1,
+}
+
+export const enum Navigator {
+  home = 0,
+  statistics = 1,
+  suites = 2,
+  notifications = 3,
+  available = 4,
+  rented = 5,
+}
+
+export const enum Navigator2 {
+  logo = 0,
+  user = 1,
+  message = 2,
+  more = 3,
+  search = 4,
+  logout = 5,
+}
